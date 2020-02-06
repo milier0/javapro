a97b0c00fcb909dcc42b5fbe1ca4b91dc8404d11
hy <837686608@qq.com>
2020-02-06 16:05:41 +0800

    提交了一个新文件

---
+++ src/com/neuedu/test/MyTest2.java
@@ -0,0 +1,10 @@
+package com.neuedu.test;
+
+/**
+ * Created by hy on 2020/2/6.
+ */
+public class MyTest2 {
+    public static void main(String[] args) {
+        int a = 10;
+    }
+}
---
+++ src/com/neuedu/test/MyTset1.java
@@ -0,0 +1,10 @@
+package com.neuedu.test;
+
+/**
+ * Created by hy on 2020/2/6.
+ */
+public class MyTset1 {
+    public static void main(String[] args) {
+        int b = 20;
+    }
+}
